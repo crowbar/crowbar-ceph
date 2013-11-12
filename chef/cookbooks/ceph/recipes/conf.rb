3b9c1b609e3919037fb4b4f6585182b2e3f195ff
Michal Jura <mjura@suse.com>
2013-11-12 14:48:53 +0100

    Fixing bug with public and cluster network

--- chef/cookbooks/ceph/recipes/conf.rb
+++ chef/cookbooks/ceph/recipes/conf.rb
@@ -8,10 +8,30 @@ mon_nodes.each do |monitor|
     mon_init << monitor.name.split('.')[0]
 end
 
-is_rgw = false
-if node['roles'].include? 'ceph-radosgw'
-  is_rgw = true
+def mask_to_bits(mask)
+  octets = mask.split(".")
+  count = 0
+  octets.each do |octet|
+    break if octet == "0"
+    c = 1 if octet == "128"
+    c = 2 if octet == "192"
+    c = 3 if octet == "224"
+    c = 4 if octet == "240"
+    c = 5 if octet == "248"
+    c = 6 if octet == "252"
+    c = 7 if octet == "254"
+    c = 8 if octet == "255"
+    count = count + c
+  end
+
+  count
 end
+
+public_mask = mask_to_bits(Chef::Recipe::Barclamp::Inventory.get_network_by_type(node, "admin").netmask)
+cluster_mask = mask_to_bits(Chef::Recipe::Barclamp::Inventory.get_network_by_type(node, "storage").netmask)
+
+public_network = Chef::Recipe::Barclamp::Inventory.get_network_by_type(node, "admin").subnet + "/#{public_mask}"
+cluster_network = Chef::Recipe::Barclamp::Inventory.get_network_by_type(node, "storage").subnet + "/#{cluster_mask}"
 
 directory "/etc/ceph" do
   owner "root"
@@ -25,7 +45,8 @@ template '/etc/ceph/ceph.conf' do
   variables(
     :mon_initial => mon_init,
     :mon_addresses => mon_addresses,
-    :is_rgw => is_rgw
+    :public_network => public_network,
+    :cluster_network => cluster_network
   )
   mode '0644'
 end
