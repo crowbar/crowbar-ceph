b278bc24c8545cba63389d32d4734a9e9e743f87
Jiří Suchomel <jsuchome@suse.cz>
2014-06-23 14:23:43 +0200

    Use SSD disk for journal device
    use rotational info read by ohai plugin
    
    default value for new partition start
    
    --journal-dev does not use device as argument; real journal device comes to the end of command
    
    bigger default value of journal size
    
    spread the journals roughly equally among available SSD devices
    
    make it configurable if SSD should be used for journal
    
    added new osd settings to schema
    
    use ceph-disk to create partition on given device
    
    for first avaialble, do not add extra journal disk
    allow user to specify list of journal partitions

--- chef/cookbooks/ceph/attributes/osd.rb
+++ chef/cookbooks/ceph/attributes/osd.rb
@@ -5,3 +5,10 @@ else
   default["ceph"]["osd"]["init_style"] = "sysvinit"
 end
 default["ceph"]["osd"]["secret_file"] = "/etc/chef/secrets/ceph_osd"
+default["ceph"]["osd"]["journal_size"] = 5120
+
+# if SSD should be automatically detected and used for journals
+default["ceph"]["osd"]["use_ssd_for_journal"] = true
+
+# list of devices to be used for journals (implies use_ssd_for_journal => false)
+default["ceph"]["osd"]["journal_devices"] = []
