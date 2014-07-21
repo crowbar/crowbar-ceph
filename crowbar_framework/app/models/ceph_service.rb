crowbar/crowbar-ceph

commit c51c9c701921d2e405d2d175656b33b375e7ad63

Only allocate an IP address on storage network for OSD nodes
The OSD nodes are the only ones requiring access to it.

http://ceph.com/docs/master/rados/configuration/network-config-ref/

crowbar_framework/app/models/ceph_service.rb
@@ -101,7 +101,7 @@ class CephService < ServiceObject
     # Make sure to use the storage network
     net_svc = NetworkService.new @logger
 
-    all_nodes.each do |n|
+    osd_nodes.each do |n|
       net_svc.allocate_ip "default", "storage", "host", n
     end
 

commit 0bed062911912626adc6714b3fd9641cac3eeec2

Fix regression introduced in c51c9c7
Not all nodes might be on the storage network anymore, so we could crash
with:

Exception undefined method `[]' for nil:NilClass /opt/dell/crowbar_framework/app/models/ceph_service.rb:117:in `apply_role_pre_chef_call'
/opt/dell/crowbar_framework/app/models/service_object.rb:1213:in `apply_role'
/opt/dell/crowbar_framework/app/models/service_object.rb:1535:in `active_update'
/opt/dell/crowbar_framework/app/models/service_object.rb:726:in `proposal_commit'
/opt/dell/crowbar_framework/app/controllers/barclamp_controller.rb:391:in `proposal_update'

crowbar_framework/app/models/ceph_service.rb
@@ -107,7 +107,7 @@ class CephService < ServiceObject
 
     # Save net info in attributes if we're applying
     unless all_nodes.empty?
-      node = NodeObject.find_node_by_name all_nodes[0]
+      node = NodeObject.find_node_by_name osd_nodes[0]
       admin_net = node.get_network_by_type("admin")
       cluster_net = node.get_network_by_type("storage")
 