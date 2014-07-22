5ec06d2fc1e77320e1e4b332903da965d46c0b75
Vincent Untz <vuntz@suse.com>
2014-07-22 10:31:10 +0200

    Use ceph_client chef resource to create the client for glance

--- chef/cookbooks/ceph/recipes/glance.rb
+++ chef/cookbooks/ceph/recipes/glance.rb
@@ -32,27 +32,15 @@ end
 glance_user = node[:glance][:rbd][:store_user]
 glance_pool = node[:glance][:rbd][:store_pool]
 
-ruby_block "save #{glance_user} key in node attributes" do
-  block do
-    client_key = %x[
-      ceph \
-        auth get-or-create-key client.#{glance_user} mon 'allow r' \
-        osd 'allow class-read object_prefix rbd_children, allow rwx pool=#{glance_pool}'
-    ].tr("\n","")
-    raise 'adding or getting glance client key failed' unless $?.exitstatus == 0
-    %x[ ceph-authtool /etc/ceph/ceph.client.#{glance_user}.keyring --create-keyring \
-          --name=client.#{glance_user} --add-key='#{client_key}' ]
-    raise 'creating glance keyring failed' unless $?.exitstatus == 0
-    node.normal['ceph']['glance-secret'] = client_key
-    node.save
-  end
-end
+ceph_caps = { 'mon' => 'allow r', 'osd' => "allow class-read object_prefix rbd_children, allow rwx pool=#{glance_pool}" }
 
-file "/etc/ceph/ceph.client.#{glance_user}.keyring" do
+ceph_client glance_user do
+  caps ceph_caps
+  keyname "client.#{glance_user}"
+  filename "/etc/ceph/ceph.client.#{glance_user}.keyring"
   owner "root"
   group node[:glance][:group]
   mode 0640
-  action :touch
 end
 
 execute "create new pool #{glance_pool}" do
