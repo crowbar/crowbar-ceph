f639bcebdc9ab8ac966adc557c216c91ffd106ad
Vincent Untz <vuntz@suse.com>
2014-07-22 10:07:52 +0200

    Use ceph_client chef resource to create the clients for cinder

--- chef/cookbooks/ceph/recipes/cinder.rb
+++ chef/cookbooks/ceph/recipes/cinder.rb
@@ -29,6 +29,8 @@ if !File.exists?(keyring)
 
 end
 
+ceph_clients = {}
+
 node[:cinder][:volumes].each_with_index do |volume, volid|
   next unless (volume['backend_driver'] == "rbd") && volume['rbd']['use_crowbar']
 
@@ -37,50 +39,35 @@ node[:cinder][:volumes].each_with_index do |volume, volid|
   cinder_user = volume[:rbd][:user]
   cinder_pool = volume[:rbd][:pool]
 
-  ruby_block "save cinder key in node attributes (#{backend_id})" do
-    block do
-
-      glance_servers = search(:node, "roles:glance-server")
-      if glance_servers.length > 0
-        glance_pool = glance_servers[0][:glance][:rbd][:store_pool]
-
-        client_key = %x[
-          ceph \
-            auth get-or-create-key client.'#{cinder_user}' mon 'allow r' \
-            osd 'allow class-read object_prefix rbd_children, allow rwx pool='#{cinder_pool}', allow rwx pool='#{glance_pool}''
-        ].tr("\n","")
-        raise 'adding or getting cinder client key failed' unless $?.exitstatus == 0
-
-      else
-
-        client_key = %x[
-          ceph \
-            auth get-or-create-key client.'#{cinder_user}' mon 'allow r' \
-            osd 'allow class-read object_prefix rbd_children, allow rwx pool='#{cinder_pool}''
-        ].tr("\n","")
-        raise 'adding or getting cinder client key failed' unless $?.exitstatus == 0
-
-      end
-
-      %x[ ceph-authtool /etc/ceph/ceph.client.'#{cinder_user}'.keyring --create-keyring \
-            --name=client.'#{cinder_user}' --add-key='#{client_key}' ]
-      raise 'creating cinder keyring failed' unless $?.exitstatus == 0
-
-      node.normal['ceph']['cinder-secret'] = client_key
-      node.save
-
-    end
-  end
-
-  file "/etc/ceph/ceph.client.#{cinder_user}.keyring (#{backend_id})" do
-    path "/etc/ceph/ceph.client.#{cinder_user}.keyring"
-    owner "root"
-    group node[:cinder][:group]
-    mode 0640
-    action :create
-  end
+  cinder_pools = (ceph_clients[cinder_user] || []) << cinder_pool
+  ceph_clients[cinder_user] = cinder_pools
 
   execute "create new pool #{cinder_pool} (#{backend_id})" do
     command "ceph osd pool create #{cinder_pool} 128"
+  end
+end
+
+unless ceph_clients.empty?
+  glance_servers = search(:node, "roles:glance-server")
+  if glance_servers.length > 0
+    glance_pool = glance_servers[0][:glance][:rbd][:store_pool]
+  else
+    glance_pool = nil
+  end
+
+  ceph_clients.each_pair do |cinder_user, cinder_pools|
+    cinder_pools << glance_pool unless glance_pool.nil?
+
+    allow_pools = cinder_pools.map{|p| "allow rwx pool=#{p}"}.join(", ")
+    ceph_caps = { 'mon' => 'allow r', 'osd' => "allow class-read object_prefix rbd_children, #{allow_pools}" }
+
+    ceph_client cinder_user do
+      caps ceph_caps
+      keyname "client.#{cinder_user}"
+      filename "/etc/ceph/ceph.client.#{cinder_user}.keyring"
+      owner "root"
+      group node[:cinder][:group]
+      mode 0640
+    end
   end
 end
