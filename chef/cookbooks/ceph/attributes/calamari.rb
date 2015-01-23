0de82bd90ae98822684cc90a0d11c8c12bb57b4e
Tim Serong <tserong@suse.com>
2015-01-23 19:29:11 +1100

    Remove platform check for calamari-clients package
    Signed-off-by: Tim Serong <tserong@suse.com>

--- chef/cookbooks/ceph/attributes/calamari.rb
+++ chef/cookbooks/ceph/attributes/calamari.rb
@@ -1,7 +1,1 @@
-case node['platform_family']
-  when 'suse'
-    # this pulls in calamari-server and all other deps
-    default['ceph']['calamari']['packages'] = ['calamari-clients']
-  else
-    default['ceph']['calamari']['packages'] = []
-end
+default['ceph']['calamari']['packages'] = ['calamari-clients']
