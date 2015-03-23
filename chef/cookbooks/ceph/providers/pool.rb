c9f9e05a7982add97554c8fa963404dc69e60b3c
Michal Jura <mjura@suse.com>
2015-03-23 10:31:22 +0100

    Add full escaping for shell commands usage

--- chef/cookbooks/ceph/providers/pool.rb
+++ chef/cookbooks/ceph/providers/pool.rb
@@ -16,7 +16,7 @@ end
 
 def get_pg_num(ceph_conf, admin_keyring)
   Chef::Log.info("Getting pg_num from ceph rbd pool")
-  cmd = "ceph -k #{admin_keyring} -c #{ceph_conf} osd pool get rbd pg_num"
+  cmd = ["ceph", "-k", admin_keyring, "-c", ceph_conf, "osd", "pool", "get", "rbd", "pg_num"]
   get_pg_num = Mixlib::ShellOut.new(cmd)
   pg_num = get_pg_num.run_command.stdout.split.last
   get_pg_num.error!
@@ -24,8 +24,8 @@ def get_pg_num(ceph_conf, admin_keyring)
 end
 
 def create_pool(ceph_conf, admin_keyring, pool_name, pg_num)
-  Chef::Log.info("Creating ceph pool '#{pool_name}' with pg number '#{pg_num}'")
-  cmd = "ceph -k #{admin_keyring} -c #{ceph_conf} osd pool create '#{pool_name}' '#{pg_num}'"
+  Chef::Log.info("Creating ceph pool #{pool_name} with pg number #{pg_num}")
+  cmd = ["ceph", "-k", admin_keyring, "-c", ceph_conf, "osd", "pool", "create", pool_name, pg_num]
   create_pool = Mixlib::ShellOut.new(cmd)
   create_pool.run_command
   create_pool.error!
