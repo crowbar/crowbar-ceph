9cd0e77cc25c003b9f89ebfaa8ff35403d5621d1
Dirk Mueller <dmueller@suse.com>
2015-10-02 09:49:09 +0200

    Unbreak Ceph again
    We do want to set a mon attribute not an osd attribute.

--- chef/cookbooks/ceph/attributes/mon.rb
+++ chef/cookbooks/ceph/attributes/mon.rb
@@ -1,8 +1,8 @@
-default["ceph"]["osd"]["init_style"] = "sysvinit"
+default["ceph"]["mon"]["init_style"] = "sysvinit"
 if node["platform_family"] == "suse"
-  default["ceph"]["osd"]["init_style"] = "systemd"
+  default["ceph"]["mon"]["init_style"] = "systemd"
 elsif node["platform"] == "ubuntu"
-  default["ceph"]["osd"]["init_style"] = "upstart"
+  default["ceph"]["mon"]["init_style"] = "upstart"
 end
 
 default["ceph"]["mon"]["secret_file"] = "/etc/chef/secrets/ceph_mon"
